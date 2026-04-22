d2beec9a3e94077d8d16332e48ed9a83e8f968a6
Sushma Yella <spoorthysushma@gmail.com>
2026-04-22 12:51:29 -0700

    feat(ingestion-api): initial Spring Boot skeleton with actuator + prometheus

---
+++ services/ingestion-api/src/main/java/com/fraudplatform/ingestion/IngestionApiApplication.java
@@ -0,0 +1,11 @@
+package com.fraudplatform.ingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestionApiApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(IngestionApiApplication.class, args);
+  }
+}
